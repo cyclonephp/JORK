4d0869deaab3066667c45ec23a21c352720d3a9f
Bence Eros <crystal@cyclonephp.com>
2012-02-21 13:05:03 +0100

    added primary key existence validation and one-to-one join column validation

--- classes/cyclone/jork/schema/SchemaValidator.php
+++ classes/cyclone/jork/schema/SchemaValidator.php
@@ -36,6 +36,7 @@ class SchemaValidator {
 
     private $_validators = array(
         array(__CLASS__, 'test_primary_keys')
+        , array(__CLASS__, 'test_table_name')
     );
 
     public function validate() {
@@ -67,8 +68,64 @@ class SchemaValidator {
         return $result;
     }
 
-    private static function test_primary_keys($schemas) {
+    public static function test_primary_keys($schemas) {
         $rval = new ValidationResult;
+        foreach ($schemas as $schema) {
+            try {
+                $schema->primary_key();
+            } catch (jork\Exception $ex) {
+                $rval->add_error("class {$schema->class} doesn't have primary key property");
+            }
+        }
+        return $rval;
+    }
+
+    public static function test_table_name($schemas) {
+        $rval = new ValidationResult;
+        foreach ($schemas as $schema) {
+            if (empty($schema->table)) {
+                $rval->add_error("class {$schema->class} does not have table");
+            }
+        }
+        return $rval;
+    }
+
+    public static function test_component_foreign_keys($schemas) {
+        $rval = new ValidationResult;
+        foreach ($schemas as $schema) {
+            foreach ($schema->components as $comp_schema) {
+                if (empty($comp_schema->mapped_by)) {
+                    switch($comp_schema->type) {
+                        case cy\JORK::ONE_TO_ONE:
+                            $join_column = empty($comp_schema->join_column)
+                                ? $comp_schema->primary_key()
+                                : $comp_schema->join_column;
+                            if ( ! isset($schema->primitives[$join_column])) {
+                                $rval->add_error('local join column ' . $schema->class
+                                        . '::$' . $comp_schema->join_column
+                                        . ' doesn\'t exist');
+                            }
+                            $inverse_schema = $schemas[$comp_schema->class];
+                            $inverse_join_col = empty($comp_schema->inverse_join_column)
+                                    ? $inverse_schema->primary_key()
+                                    : $comp_schema->inverse_join_column;
+                            if ( ! isset($inverse_schema->primitives[$inverse_join_col])) {
+                                $rval->add_error('inverse join column ' . $comp_schema->class
+                                        . '::$' . $comp_schema->inverse_join_column
+                                        . ' doesn\'t exist');
+                            }
+                            break;
+                        case cy\JORK::ONE_TO_MANY:
+                            break;
+                        case cy\JORK::MANY_TO_ONE:
+                            break;
+                        case cy\JORK::MANY_TO_MANY:
+                            break;
+                    }
+                }
+            }
+        }
+        return $rval;
     }
 
 }
--- tests/jork/schema/ValidatorTest.php
+++ tests/jork/schema/ValidatorTest.php
@@ -1,10 +1,46 @@
 <?php
+use cyclone as cy;
+use cyclone\jork\schema;
+use cyclone\jork;
 
 class Schema_ValidatorTest extends Kohana_Unittest_TestCase {
 
     public function testTableNameTest() {
-        
-        $this->assertTrue(false);
+        $schema = new schema\ModelSchema;
+        $schema->class = 'TestModel';
+        $result = schema\SchemaValidator::test_table_name(array($schema));
+        $this->assertEquals(array(
+            "class {$schema->class} does not have table"
+        ), $result->error);
+    }
+
+    public function testPrimaryKeyTest() {
+        $schema = new schema\ModelSchema;
+        $schema->class = 'TestModel';
+        $result = schema\SchemaValidator::test_primary_keys(array($schema));
+        $this->assertEquals(array(
+            "class {$schema->class} doesn't have primary key property"
+        ), $result->error);
+    }
+
+    
+
+    public function testJoinColumnTest() {
+        $schema1 = new schema\ModelSchema;
+        $schema1->class = 'TestModel1';
+        $schema1->component(cy\JORK::component('model2', 'TestModel2')
+                ->type(cy\JORK::ONE_TO_ONE)->join_column('m1_id')
+                ->inverse_join_column('m2_id'));
+        $schema2 = new schema\ModelSchema;
+        $schema2->class = 'TestModel2';
+        $rval = schema\SchemaValidator::test_component_foreign_keys(array(
+            'TestModel1' => $schema1
+            , 'TestModel2' => $schema2
+        ));
+        $this->assertEquals(array(
+            'local join column TestModel1::$m1_id doesn\'t exist',
+            'inverse join column TestModel2::$m2_id doesn\'t exist'
+        ), $rval->error);
     }
     
 }
