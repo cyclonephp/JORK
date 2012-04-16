c54e93a3cc7221de33c634c6f2a0d8f7adc20f0c
Bence Eros <crystal@cyclonephp.org>
2012-04-16 23:46:26 +0200

    finished refactoring tests in JORK_Mapper_SelectTest to use JORK_MapperTest::assertCompiledTo()

--- tests/JORK/Mapper/SelectTest.php
+++ tests/JORK/Mapper/SelectTest.php
@@ -116,48 +116,33 @@ class JORK_Mapper_SelectTest extends JORK_MapperTest {
     }
 
     public function testOneToOne() {
-        $jork_query = new query\SelectQuery;
-        $jork_query->select('moderator')->from('Model_Category');
-        $mapper = jork\mapper\SelectMapper::for_query($jork_query);
-        list($db_query, ) = $mapper->map();
-        $this->assertEquals($db_query->tables, array(
-            array('t_categories', 't_categories_0')
-        ));
-        $this->assertEquals($db_query->joins, array(
-            array(
-                'table' => array('t_users', 't_users_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('t_categories_0.moderator_fk', '=', 't_users_0.id')
-                )
-            ),
-            array(
-                'table' => array('user_contact_info', 'user_contact_info_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('t_users_0.id', '=', 'user_contact_info_0.user_fk')
-                )
-            )
-        ));
+        $jork_query = cy\JORK::select('moderator')->from('Model_Category');
+        $db_query = cy\DB::select(array('t_users_0.id', 't_users_0_id')
+            , array('t_users_0.name', 't_users_0_name')
+            , array('t_users_0.password', 't_users_0_password')
+            , array('t_users_0.created_at', 't_users_0_created_at')
+            , array('user_contact_info_0.email', 'user_contact_info_0_email')
+            , array('user_contact_info_0.phone_num', 'user_contact_info_0_phone_num')
+            , array('t_categories_0.id', 't_categories_0_id'))->from(array('t_categories', 't_categories_0'))
+            ->left_join(array('t_users', 't_users_0'))
+                ->on('t_categories_0.moderator_fk', '=', 't_users_0.id')
+            ->left_join(array('user_contact_info', 'user_contact_info_0'))
+                ->on('t_users_0.id', '=', 'user_contact_info_0.user_fk');
+
+        $this->assertCompiledTo($jork_query, $db_query);
     }
 
     public function testOneToOneReverse() {
-        $jork_query = new query\SelectQuery;
-        $jork_query->select('moderated_category')->from('Model_User');
-        $mapper = jork\mapper\SelectMapper::for_query($jork_query);
-        list($db_query, ) = $mapper->map();
-        $this->assertEquals($db_query->tables, array(
-            array('t_users', 't_users_0')
-        ));
-        $this->assertEquals($db_query->joins, array(
-            array(
-                'table' => array('t_categories', 't_categories_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('t_users_0.id', '=', 't_categories_0.moderator_fk')
-                )
-            )
-        ));
+        $jork_query = cy\JORK::select('moderated_category')->from('Model_User');
+        $db_query = cy\DB::select(array('t_categories_0.id', 't_categories_0_id')
+            , array('t_categories_0.c_name', 't_categories_0_c_name')
+            , array('t_categories_0.moderator_fk', 't_categories_0_moderator_fk')
+            , array('t_users_0.id', 't_users_0_id')
+        )->from(array('t_users', 't_users_0'))
+            ->left_join(array('t_categories', 't_categories_0'))
+                ->on('t_users_0.id', '=', 't_categories_0.moderator_fk')
+        ->order_by('t_users_0.name', 'asc');
+        $this->assertCompiledTo($jork_query, $db_query);
     }
 
     /**
@@ -171,54 +156,33 @@ class JORK_Mapper_SelectTest extends JORK_MapperTest {
 
     public function testManyToMany() {
         $jork_query = cy\JORK::from('Model_Topic')->with('categories');
-        $mapper = jork\mapper\SelectMapper::for_query($jork_query);
-        list($db_query, ) = $mapper->map();
-        $this->assertEquals($db_query->tables, array(
-            array('t_topics', 't_topics_0')
-        ));
-        $this->assertEquals($db_query->joins, array(
-            array(
-                'table' => array('categories_topics', 'categories_topics_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('t_topics_0.id', '=', 'categories_topics_0.topic_fk')
-                )
-            ),
-            array(
-                'table' => array('t_categories', 't_categories_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('categories_topics_0.category_fk', '=', 't_categories_0.id')
-                )
-            )
-        ));
+        $db_query = cy\DB::select(array('t_categories_0.id', 't_categories_0_id')
+            , array('t_categories_0.c_name', 't_categories_0_c_name')
+            , array('t_categories_0.moderator_fk', 't_categories_0_moderator_fk')
+            , array('t_topics_0.id', 't_topics_0_id')
+            , array('t_topics_0.name', 't_topics_0_name'))->from(array('t_topics', 't_topics_0'))
+            ->left_join(array('categories_topics', 'categories_topics_0'))
+                ->on('t_topics_0.id', '=', 'categories_topics_0.topic_fk')
+            ->left_join(array('t_categories', 't_categories_0'))
+                ->on('categories_topics_0.category_fk', '=', 't_categories_0.id')
+        ->order_by('t_topics_0.name', 'asc');
+
+        $this->assertCompiledTo($jork_query, $db_query);
     }
 
     public function testManyToManyReverse() {
         $jork_query = cy\JORK::from('Model_Category')->with('topics');
-        $mapper = jork\mapper\SelectMapper::for_query($jork_query);
-        list($db_query, ) = $mapper->map();
-        $this->assertEquals($db_query->tables, array(
-            array('t_categories', 't_categories_0')
-        ));
-        $this->assertEquals($db_query->joins, array(
-            array(
-                'table' => array('categories_topics', 'categories_topics_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('t_categories_0.id'
-                            , '=', 'categories_topics_0.category_fk')
-                )
-            ),
-            array(
-                'table' => array('t_topics', 't_topics_0'),
-                'type' => 'LEFT',
-                'conditions' => array(
-                    new db\BinaryExpression('categories_topics_0.topic_fk'
-                            , '=', 't_topics_0.id')
-                )
-            )
-        ));
+        $db_query = cy\DB::select(array('t_topics_0.id', 't_topics_0_id')
+            , array('t_topics_0.name', 't_topics_0_name')
+            , array('t_categories_0.id', 't_categories_0_id')
+            , array('t_categories_0.c_name', 't_categories_0_c_name')
+            , array('t_categories_0.moderator_fk', 't_categories_0_moderator_fk')
+        )->from(array('t_categories', 't_categories_0'))
+            ->left_join(array('categories_topics', 'categories_topics_0'))
+                ->on('t_categories_0.id', '=', 'categories_topics_0.category_fk')
+            ->left_join(array('t_topics', 't_topics_0'))
+                ->on('categories_topics_0.topic_fk', '=', 't_topics_0.id');
+        $this->assertCompiledTo($jork_query, $db_query);
     }
 
 
