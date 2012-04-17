24fd9e58a54ef01d07e0773d7e5bf4bd1fcd0510
Bence Eros <crystal@cyclonephp.org>
2012-04-17 22:21:02 +0200

    changed OneToOneFKBuilder and OneToManyFKBuilder to support composite foreign keys

--- classes/cyclone/jork/schema/foreignkey/OneToManyFKBuilder.php
+++ classes/cyclone/jork/schema/foreignkey/OneToManyFKBuilder.php
@@ -12,57 +12,71 @@ class OneToManyFKBuilder extends ForeignKeyBuilder {
 
     public function create_foreign_key() {
         $comp_class_schema = $this->_schema_pool[$this->_comp_schema->class];
-        $foreign_table = NULL;
-        $foreign_col = NULL;
+        $foreign_tables = array();
+        $foreign_cols = array();
         foreach ($comp_class_schema->primitives as $prim_schema) {
             $col_name = NULL === $prim_schema->column ? $prim_schema->name : $prim_schema->column;
-            if ($col_name === $this->_comp_schema->join_column) {
+            if (in_array($col_name, $this->_comp_schema->join_columns)) {
                 $table_name = NULL === $prim_schema->table
                         ? $comp_class_schema->table
                         : $prim_schema->table;
-                $foreign_table = schema\Table::get_by_name($table_name);
+                $foreign_table = ($foreign_tables []= schema\Table::get_by_name($table_name));
                 if (!isset($this->_table_pool[$foreign_table->name])) {
                     $this->_table_pool[$foreign_table->name] = $foreign_table;
                 }
-                $foreign_col = $foreign_table->get_column($col_name);
+                $foreign_cols []= $foreign_table->get_column($col_name);
             }
         }
-        if (NULL === $foreign_table)
+        if (empty($foreign_tables))
             throw new SchemaBuilderException("Failed to create foreign key constraint: "
                     . "no foreign table found for {$this->_model_schema->class}::\${$comp_schema->name}");
-        $fk = new schema\ForeignKey;
-        $fk->local_table = $foreign_table;
-        // TODO composite foreign key handling
-        $fk->local_columns = array($foreign_col);
 
-        $pk_schema = $this->_model_schema->primitives[$this->_model_schema->primary_key()];
-        if (NULL === $this->_comp_schema->inverse_join_column) {
-            $local_col_name = NULL === $pk_schema->column
-                    ? $pk_schema->name
-                    : $pk_schema->column;
-        } else {
-            $local_col_name = $comp_schema->inverse_join_column;
-        }
-        $local_table = NULL;
-        foreach ($this->_model_schema->primitives as $prim_schema) {
-            $tmp_col_name = NULL === $prim_schema->column
+        $fks = array();
+        $comp_schema = $this->_comp_schema;
+        foreach ($foreign_tables as $idx => $foreign_table) {
+            $foreign_col = $foreign_cols[$idx];
+            if ( ! isset($fks[$foreign_table->name])) {
+                $fks[$foreign_table->name] = new schema\ForeignKey();
+            }
+            $fk = $fks[$foreign_table->name];
+            $fk->local_columns []= $foreign_col;
+
+            $pk_prop_names = array($this->_model_schema->primary_key());
+            $pk_schemas = array();
+            foreach ($pk_prop_names as $pk_prop_name) {
+                $pk_schemas []= $this->_model_schema->primitives[$pk_prop_name];
+            }
+            $local_col_names = array();
+            if (empty($this->_comp_schema->inverse_join_columns)) {
+                foreach ($pk_schemas as $pk_schema) {
+                    $local_col_names []= NULL === $pk_schema->column
+                        ? $pk_schema->name
+                        : $pk_schema->column;
+                }
+            } else {
+                $local_col_names = $comp_schema->inverse_join_columns;
+            }
+
+            foreach($this->_model_schema->primitives as $prim_schema) {
+                $tmp_col_name = NULL === $prim_schema->column
                     ? $prim_schema->name
                     : $prim_schema->column;
-            if ($tmp_col_name === $local_col_name) {
-                $local_table = schema\Table::get_by_name(NULL === $prim_schema->table
+                if (in_array($tmp_col_name, $local_col_names)) {
+                    $local_table = schema\Table::get_by_name(NULL === $prim_schema->table
                         ? $this->_model_schema->table
                         : $prim_schema->table);
-                if (!isset($this->_table_pool[$local_table->name])) {
-                    $this->_table_pool[$local_table->name] = $local_table;
+                    if ( ! isset($this->_table_pool[$local_table->name])) {
+                        $this->_table_pool[$local_table->name] = $local_table;
+                    }
+                    $local_col = $local_table->get_column($tmp_col_name);
+                    $fk->foreign_table = $local_table;
+                    $fk->foreign_columns []= $local_col;
                 }
-                $local_col = $local_table->get_column($local_col_name);
-                break;
             }
         }
-        $fk->foreign_table = $local_table;
-        $fk->foreign_columns = array($local_col);
-
-        $this->_table_pool[$fk->local_table->name]->add_foreign_key($fk);
+        foreach ($fks as $tbl_name => $fk) {
+            schema\Table::get_by_name($tbl_name)->add_foreign_key($fk);
+        }
     }
 
 }
--- classes/cyclone/jork/schema/foreignkey/OneToOneFKBuilder.php
+++ classes/cyclone/jork/schema/foreignkey/OneToOneFKBuilder.php
@@ -11,39 +11,53 @@ use cyclone\db\schema;
 class OneToOneFKBuilder extends ForeignKeyBuilder {
 
     public function create_foreign_key() {
-        $local_column = $this->_comp_schema->join_column;
-        $local_table = $this->_model_schema->table_name_for_column($local_column);
-        if ($local_column === $this->_comp_schema->join_column
-                || ($local_column !== $this->_comp_schema->join_column
-                    && $this->_model_schema->primary_key_strategy() === cy\JORK::ASSIGN)) {
-            $fk = new schema\ForeignKey;
-            $fk->local_table = schema\Table::get_by_name($local_table);
-            if ( ! isset($this->_table_pool[$local_table])) {
-                $this->_table_pool[$local_table] = $fk->local_table;
+        $join_columns = $this->_comp_schema->join_columns;
+        $local_tables = $this->_model_schema->table_names_for_columns($join_columns);
+        $local_fks = array();
+        foreach ($join_columns as $idx => $local_column) {
+            //$local_tables[$idx] = $local_table = $this->_model_schema->table_name_for_column($local_column);
+            if (($join_columns == $this->_comp_schema->join_columns)
+                || ($this->_model_schema->primary_key_strategy() === cy\JORK::ASSIGN)) {
+                $local_table = $local_tables[$idx];
+                if ( ! isset($local_fks[$local_table])) {
+                    $local_fks[$local_table] = new schema\ForeignKey();
+                    $local_fks[$local_table]->local_table = schema\Table::get_by_name($local_table);
+                }
+                $fk = $local_fks[$local_table];
+                if ( ! isset($this->_table_pool[$local_table])) {
+                    $this->_table_pool[$local_table] = $fk->local_table;
+                }
+                $fk->local_columns []= $fk->local_table->get_column($local_column);
             }
-            $fk->local_columns = array($fk->local_table->get_column($local_column));
+        }
+
+        foreach ($local_fks as $local_table => $fk) {
             $this->_table_pool[$local_table]->add_foreign_key($fk);
         }
 
         $comp_class_schema = $this->_schema_pool[$this->_comp_schema->class];
-        $inv_column = $this->_comp_schema->inverse_join_column;
-        $inv_table = $comp_class_schema->table_name_for_column($inv_column);
-        if ($inv_column !== $this->_comp_schema->inverse_join_column
-                || $comp_class_schema->primary_key_strategy() == cy\JORK::ASSIGN) {
-            $fk = new schema\ForeignKey;
-            $fk->local_table = schema\Table::get_by_name($inv_table);
-            if ( ! isset($this->_table_pool[$inv_table])) {
-                $this->_table_pool[$inv_table] = $fk->local_table;
-            }
-            $fk->local_columns = array($fk->local_table->get_column($inv_column));
 
-            $fk->foreign_table = schema\Table::get_by_name($local_table);
-            if ( ! isset($this->_table_pool[$local_table])) {
-                $this->_table_pool[$local_table] = $fk->foreign_table;
+        $inv_join_cols = $this->_comp_schema->inverse_join_columns;
+        $inv_tables = $comp_class_schema->table_names_for_columns($inv_join_cols);
+        foreach ($inv_join_cols as $idx => $inv_column) {
+            $inv_table = $inv_tables[$idx];
+         /*   if ($inv_column !== $this->_comp_schema->inverse_join_columns[$idx]
+                    || $comp_class_schema->primary_key_strategy() == cy\JORK::ASSIGN) {*/
+                $fk = new schema\ForeignKey;
+                $fk->local_table = schema\Table::get_by_name($inv_table);
+                if ( ! isset($this->_table_pool[$inv_table])) {
+                    $this->_table_pool[$inv_table] = $fk->local_table;
+                }
+                $fk->local_columns = array($fk->local_table->get_column($inv_column));
+
+                $fk->foreign_table = schema\Table::get_by_name($local_tables[$idx]);
+                if ( ! isset($this->_table_pool[$local_tables[$idx]])) {
+                    $this->_table_pool[$local_tables[$idx]] = $fk->foreign_table;
+                }
+                $fk->foreign_columns = array($fk->foreign_table->get_column($local_column));
+                $this->_table_pool[$inv_table]->add_foreign_key($fk);
             }
-            $fk->foreign_columns = array($fk->foreign_table->get_column($local_column));
-            $this->_table_pool[$inv_table]->add_foreign_key($fk);
-        }
+        //}
     }
 
 }
