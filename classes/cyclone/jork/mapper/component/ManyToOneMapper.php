1155ff2effa1a8c3b669a738d464fed9ddc4fea9
Bence Eros <crystal@cyclonephp.org>
2012-04-22 01:19:56 +0200

    fixing ManyToOneMapper to support composite foreign keys

--- classes/cyclone/jork/mapper/component/ManyToOneMapper.php
+++ classes/cyclone/jork/mapper/component/ManyToOneMapper.php
@@ -62,14 +62,33 @@ class ManyToOneMapper extends AbstractMapper {
         $remote_schema = jork\model\AbstractModel::schema_by_class($remote_class);
 
         $remote_comp_def = $remote_schema->components[$local_schema->mapped_by];
+        $remote_columns = $remote_comp_def->join_columns;
+        $remote_tables = $remote_schema->table_names_for_columns($remote_columns);
 
-        $remote_join_col_def = $remote_schema->primitives[$remote_comp_def->join_column];
+        $joins = array();
 
-        $remote_join_table = isset($remote_join_col_def->table)
-                ? $remote_join_col_def->table
-                : $remote_schema->table;
-
-        $remote_table_alias = $this->table_alias($remote_join_table);
+        foreach ($remote_comp_def->join_columns as $idx => $remote_join_col) {
+            $remote_join_col_def = $remote_schema->primitives[$remote_join_col];
+            $remote_join_table = $remote_tables[$idx];
+            $remote_table_alias = $this->table_alias($remote_join_table);
+            $remote_column = $remote_columns[$idx];
+            if ( ! isset($joins[$remote_join_table])) {
+                $joins[$remote_join_table] = array(
+                    'table' => array($remote_join_table, $remote_table_alias),
+                    'type' => 'LEFT',
+                    'conditions' => array()
+                );
+                $this->_db_query->joins []= &$joins[$remote_join_table];
+            }
+            $joins[$remote_join_table]['conditions'] []= new db\BinaryExpression(
+                    $this->_naming_srv->table_alias($this->_parent_mapper->_entity_alias
+                    , $this->_parent_mapper->_entity_schema->table)
+                    .'.'
+                    .$this->_parent_mapper->_entity_schema->primary_key()
+                , '='
+                ,$remote_table_alias.'.'.$remote_column);
+        }
+return;
 
         $this->_db_query->joins []= array(
             'table' => array($remote_join_table, $remote_table_alias),
